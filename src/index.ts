3b1c0aa600ba4eb39c319560ae6681e3b26e81e6
heeeyuuu <664475324@qq.com>
2022-01-14 01:06:37 +0800

    mixclassnames

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
+/// <reference path="./index.d.ts" />
+
 const delimiter: string = " "
 const preGlobal: string = "$"
 
-type BaseTYPE = string | object
-type ParamsTYPE = BaseTYPE | BaseTYPE[]
-type ClassNamesTYPE = string[]
-
 const tools = {
 	toArray: (name: string) => name.split(/\s+/),
-	toString: (names: ClassNamesTYPE) => names.join(delimiter),
-	toNormal: (names: ClassNamesTYPE) => names.map(name => name.replace(preGlobal, ''))
+	toString: (names: CssModuleSpace.ClassName) => names.join(delimiter),
+	toNormal: (names: CssModuleSpace.ClassName) => names.map(name => name.replace(preGlobal, ''))
 }
 
 // 去除重复
-function exrepeat(params: ClassNamesTYPE): ClassNamesTYPE {
+function exrepeat(params: CssModuleSpace.ClassName): CssModuleSpace.ClassName {
 	// return [...new Set(params)]
 	return params.filter((val, index, arr) => val ? arr.indexOf(val) === index : false)
 }
 
 // 处理成局部样式
-function handlePrivateClassname(params: ClassNamesTYPE | string) {
-	const names = params instanceof Array ? params : tools.toArray(params)
-	return tools.toString(exrepeat(tools.toNormal(names)))
+function handlePrivateClassname(params: CssModuleSpace.ClassName) {
+	return tools.toString(exrepeat(tools.toNormal(params)))
 }
 
 // 处理成全局样式
@@ -29,7 +26,7 @@ function handlePublicClassname(params: string[]) {
 }
 
 // 将name转换为modules中对应的键值
-function handleModules(params: ClassNamesTYPE,modules: (string | object)[]) {
+function handleModules(params: CssModuleSpace.ClassName,modules: CssModuleSpace.Module[]): CssModuleSpace.ClassName {
 
 	const classname: string[] = []
 
@@ -49,14 +46,14 @@ function handleModules(params: ClassNamesTYPE,modules: (string | object)[]) {
 }
 
 // 处理alias
-function handleAlias(params: ClassNamesTYPE, alias?: object) {
+function handleAlias(params: CssModuleSpace.ClassName, alias?: CssModuleSpace.Alias) {
 	return params.map(name => name.replace(/\{(\w+)\}/,(_, name) => alias ? alias[name] || name : name))
 }
 
 // 将classname转换为classname[]
-function dismantleCss(params: ParamsTYPE[]) {
+function dismantleCss(params: CssModuleSpace.ClassNames) {
 
-    const transformCss = (params: ParamsTYPE): string => {
+    const transformCss = (params: CssModuleSpace.ClassNames): string => {
         if(typeof params === "string") {
 			return params
         }
@@ -74,22 +71,32 @@ function dismantleCss(params: ParamsTYPE[]) {
 	return classname
 }
 
-function classnames(...args: ParamsTYPE[]) {
+function classnames(...args: CssModuleSpace.ClassNames[]) {
 	return handlePrivateClassname(dismantleCss(args))
+}
+
+interface MixClassnames {
+	alias?: CssModuleSpace.Alias
+}
+
+function mixClassnames(config: MixClassnames) {
+	return (...args: CssModuleSpace.ClassNames[]) => {
+		return handlePrivateClassname(handleAlias(dismantleCss(args), config.alias))
+	}
 }
 
 interface CssModuleConfig {
 	strict?: boolean // 是否严格模式，严格模式下未找到的局部变量将被删除
-	alias?: object // 别名 {}
+	alias?: CssModuleSpace.Alias
 }
 
-function cssModule(params: ParamsTYPE, config: CssModuleConfig = {}) {
+function cssModule(params: CssModuleSpace.Mudules, config: CssModuleConfig = {}) {
 
 	const modules =  params instanceof Array ? params : [params]
 
-	const classnames = (...args: ParamsTYPE[]) => {
+	return (...args: CssModuleSpace.ClassNames[]) => {
 
-		const names = handleAlias(dismantleCss(args))
+		const names = handleAlias(dismantleCss(args), config.alias)
 		let classname = handleModules(names, modules)
 
 		// 严格模式
@@ -98,13 +105,12 @@ function cssModule(params: ParamsTYPE, config: CssModuleConfig = {}) {
 		}
 
 		return handlePrivateClassname(classname)
-
 	}
-	return classnames
 }
 
 export {
 	classnames, // 将classnames作为全局样式进行计算
+	mixClassnames
 }
 
 export default cssModule
