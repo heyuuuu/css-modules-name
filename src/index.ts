a5062dff97fe8608c576531f726446f4eea1d08c
何宇 <664475324@qq.com>
2022-01-13 19:48:22 +0800

    null

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 const delimiter: string = " "
 const preGlobal: string = "$"
 
-type BaseTYPE = string | object | undefined
+type BaseTYPE = string | object
 type ParamsTYPE = BaseTYPE | BaseTYPE[]
 
 const tools = {
-	toArray: (name: string) => name.match(/(\$?[\w\-]+)/g) || [],
+	toArray: (name: string) => name.split(/\s+/),
 	toString: (names: string[]) => names.join(delimiter),
 	toNormal: (names: string[]) => names.map(name => name.replace(preGlobal, ''))
 }
@@ -27,6 +27,32 @@ function handlePublicClassname(params: string[]) {
 	
 }
 
+// 处理module-name
+function handleModules(params: string[],modules: (string | object)[]) {
+
+	const classname: string[] = []
+
+	params.map(name => {
+		if(/^\$/.test(name)) {
+			classname.push(name)
+		} else {
+			modules.map(module => {
+				if(typeof module == "object" && module[name]) {
+					classname.push(module[name])
+				}
+			})
+		}
+	})
+
+	return classname
+}
+
+// 处理alias
+function handleAlias(params: string[], alias?: object) {
+	return params.map(name => name.replace(/\{(\w+)\}/,(_, name) => alias?.[name] || name))
+}
+
+// 重组classnames
 function dismantleCss(params: ParamsTYPE[]) {
 
     const transformCss = (params: ParamsTYPE): string => {
@@ -53,6 +79,7 @@ function classnames(...args: ParamsTYPE[]) {
 
 interface CssModuleConfig {
 	strict?: boolean // 是否严格模式，严格模式下未找到的局部变量将被删除
+	alias?: object // 别名 {}
 }
 
 function cssModule(params: ParamsTYPE, config: CssModuleConfig = {}) {
@@ -61,22 +88,11 @@ function cssModule(params: ParamsTYPE, config: CssModuleConfig = {}) {
 
 	const classnames = (...args: ParamsTYPE[]) => {
 
-		let classname: string[] = []
 
-		const names = dismantleCss(args)
+		const names = handleAlias(dismantleCss(args))
+		let classname = handleModules(names, modules)
 
-		names.map(name => {
-			if(/^\$/.test(name)) {
-				classname.push(name)
-			} else {
-				modules.map(module => {
-					if(typeof module == "object" && module[name]) {
-						classname.push(module[name])
-					}
-				})
-			}
-		})
-
+		// 严格模式
 		if(!config.strict) {
 			classname = classname.concat(names)
 		}
